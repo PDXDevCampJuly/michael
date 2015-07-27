b321f42cdf68ec66f0245bd8b9df67b3317d0a01
williamsmichael <willia32@hotmail.com>
2015-07-27 12:28:37 -0700

    made madlib file

---
+++ madLib.py
@@ -0,0 +1,40 @@
+# this is a madlib program
+# -------------------------
+
+# declaring variables
+prompt = " >>> "
+
+# def madLib(input):
+#   article += text + theInput
+#   return article
+
+article = "Pizza was invented by a "
+theInput = input("Please type an 'abjective'" + prompt)
+article += theInput
+# print(article)
+
+theInput = input("Please type a 'nationality'" + prompt)
+article += " " + theInput
+# print(article)
+
+article += " chef named "
+theInput = input("Please type a 'person's name'" + prompt)
+article += theInput
+# print(article)
+
+article += ". To make pizza, you need to take a lump of "
+theInput = input("Please type a 'noun'" + prompt)
+article += theInput
+# print(article)
+
+article += ", and make a thin, round "
+theInput = input("Please type a 'adjective'" + prompt)
+article += theInput
+# print(article)
+
+
+
+
+
+
+

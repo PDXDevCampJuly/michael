ea859be09e73f6303165e681ae4c0feaea4eff05
williamsmichael <willia32@hotmail.com>
2015-07-29 15:36:59 -0700

    modified encoder, v2 of decoder

--- encryption/decoder.py
+++ encryption/decoder.py
@@ -10,22 +10,38 @@ class Decoder():
   def __init__(self, fileName = "secret.txt"):
     self.fileName = fileName
     self.encodedString = self.getSecret()
-    self.detransform()
+    self.level()
+
+  # determine the level to decode
+  def level(self):
+    string = self.encodedString.split()
+    if len(string[0]) > 3:
+      if len(string[0]) > 2:
+        self.detransform_2()
+    else:
+      self.detransform_1()
 
   # make a function to "detransform" a string
-  def detransform(self):
+  def detransform_1(self):
     messageList = []
     string = self.encodedString.split()
     for each_number in string:
       messageList.append(chr(int(each_number)))
     print(''.join(messageList))
 
+  def detransform_2(self):
+    messageList = []
+    string = self.encodedString.split()
+    for each_number in string:
+      part1 = each_number[3:]
+      part2 = part1[:-2]
+      messageList.append(chr(int(part2)))
+    print(''.join(messageList))
+
   # make a function to get the secret message from the imported file
   def getSecret(self):
-
     with open(self.fileName, "r") as f:
       string = f.read()
-
     return string
 
 if __name__ == '__main__':
--- encryption/encoder.py
+++ encryption/encoder.py
@@ -11,8 +11,7 @@ class Encoder():
   def __init__(self, message = "", fileName = "secret.txt", level = 1):
     self.message = self.testInput(message)
     self.fileName = fileName
-    self.level = self.transformLevel(int(level))
-    self.encodedMessage = self.transform_1()
+    self.transformLevel(int(level))
     self.makeFile()
 
   def testInput(self, message):
@@ -22,57 +21,41 @@ class Encoder():
       message = input("Input message: ")
     return message
 
+  # determine the level to encode
   def transformLevel(self, level):
     if level == 1:
+      self.level = 1
       self.transform_1()
     elif level == 2:
+      self.level = 2
       self.transform_2()
     else:
-      level = input("Input level 1 or 2: ")
+      self.level = input("Input level 1 or 2: ")
       self.transformLevel(int(level))
 
-  # make a function transform a string level = 1
+  # make a function to transform a string level = 1
   def transform_1(self):
     messageList = []
+
     for each_letter in self.message:
       messageList.append(str(ord(each_letter)))
       string = ' '.join(messageList)
-    return string
+    print(messageList)
+    self.encodedMessage = string
 
+  # make a function to transform a string level = 2
   def transform_2(self):
     messageList = []
-    newList = []
-    lenList = []
 
     for each_letter in self.message:
-      messageList.append(each_letter)
-    lenMessageList = len(messageList)
-    list1 = list(range(0, lenMessageList))
-    list2 = list(range(0, lenMessageList))
-    list3 = list(range(0, lenMessageList))
-    list4 = list(range(0, lenMessageList))
-
-    for each_item in list1:
-      list1[each_item] = str(randint(10, 99))
-    for each_item in list2:
-      list2[each_item] = str(randint(10, 99))
-    for each_item in list3:
-      list3[each_item] = str(randint(10, 99))
-    for each_item in list4:
-      list4[each_item] = str(randint(10, 99))
-    newList = [a + b + c + d + e for a, b, c, d, e in zip(list1, list2, messageList, list3, list4)]
-    for each_item in newList:
-      lenList.append(str(len(each_item)))
-    finalList = [a + b for a, b in zip(lenList, newList)]
-
-    print(messageList)
-    print(list1)
-    print(list2)
-    print(list3)
-    print(list4)
-    print(newList)
-    print(lenList)
-    print(finalList)
+      toOrd = str(ord(each_letter))
+      random1 = str(randint(10, 99))
+      random2 = str(randint(10, 99))
+      encoded = str(len(toOrd)) + random1 + toOrd + random2
+      messageList.append(encoded)
+      string = ' '.join(messageList)
+    # print(messageList)
+    self.encodedMessage = string
 
   # make a function to make a secret fileName
   def makeFile(self):

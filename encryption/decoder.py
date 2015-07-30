6d3fd2223d37093a52f5d7b9af437891b4f382d1
williamsmichael <willia32@hotmail.com>
2015-07-29 21:52:52 -0700

    modified v2 of decoder

--- encryption/decoder.py
+++ encryption/decoder.py
@@ -9,29 +9,26 @@ class Decoder():
 
   def __init__(self, fileName = "secret.txt"):
     self.fileName = fileName
-    self.encodedString = self.getSecret()
+    self.getSecret()
     self.level()
 
   # determine the level to decode
   def level(self):
     string = self.encodedString.split()
     if len(string[0]) > 3:
-      if len(string[0]) > 2:
-        self.detransform_2()
+      self.detransform_2(string)
     else:
-      self.detransform_1()
+      self.detransform_1(string)
 
   # make a function to "detransform" a string
-  def detransform_1(self):
+  def detransform_1(self, string):
     messageList = []
-    string = self.encodedString.split()
     for each_number in string:
       messageList.append(chr(int(each_number)))
     print(''.join(messageList))
 
-  def detransform_2(self):
+  def detransform_2(self, string):
     messageList = []
-    string = self.encodedString.split()
     for each_number in string:
       part1 = each_number[3:]
       part2 = part1[:-2]
@@ -41,8 +38,7 @@ class Decoder():
   # make a function to get the secret message from the imported file
   def getSecret(self):
     with open(self.fileName, "r") as f:
-      string = f.read()
-    return string
+      self.encodedString = f.read()
 
 if __name__ == '__main__':
   my_decoder = Decoder(*argv[1:])

06ea342d0d7685390778c5cc77cfa6cd0a7e0883
williamsmichael <willia32@hotmail.com>
2015-08-04 18:21:30 -0700

    minor fixes to Account and Person classes

--- bank.py
+++ bank.py
@@ -12,7 +12,7 @@ class Account:
     self.balance = balance
     self.account_type = account_type.capitalize()
 
-    print("\nWelcome {}. {} account established. Current balance is: {}".format(self.account_type, self.print_balance()))
+    print("\nWelcome. {} account established. Current balance is: {}".format(self.account_type, self.print_balance()))
 
   def deposit(self, money):
     self.balance += money
@@ -37,7 +37,7 @@ class Person:
   """
   Person class that tracks Persons in our bank.
   """
-
+  # initializes the Person class
   def __init__(self, first_name, last_name, email):
     self.first = first_name
     self.last = last_name
@@ -45,41 +45,42 @@ class Person:
     self.accounts = {}
 
   def open_account(self, initial_balance, account_name, account_type="Checking"):
-    new_account = Account(initial_balance, account_name, account_type)
-    self.accounts[account_type] = new_account
+    new_account = Account(initial_balance, account_type)
+    self.accounts[account_name.lower()] = new_account
 
-  def close_account(self, account_type):
+  def close_account(self, account_name):
     self.show_accounts()
-    if account_type not in self.accounts:
-      print("{} account type does not exist. Try again.".format(account_type))
-    if account_type in self.accounts:
-      confirmation = input("Type YES to confirm {} account closure. NO to cancel: ".format(account_type.capitalize()))
+    if account_name.lower() not in self.accounts:
+      print(">>> '{}' account type does not exist. Try again.\n".format(account_name))
+    if account_name.lower() in self.accounts:
+      confirmation = input("Type YES to confirm {} account closure. NO to cancel: ".format(account_name.title()))
       if confirmation.lower() == "yes":
-        del self.accounts[account_type]
-        print(">>> {} account closed. Have a good day!".format(account_type.capitalize()))
+        del self.accounts[account_name.lower()]
+        print("'{}' account closed. Have a good day!\n".format(account_name.title()))
       else:
         print(">>> Transaction cancelled.")
-
 
   def show_accounts(self):
     """
     Display on the screen all of a Person's account info
     """
-
     if len(self.accounts) == 0:
-      print("No accounts established for {} {}:".format(self.first, self.last))
+      print(">>> No accounts established for {} {}\n".format(self.first, self.last))
     else:
+      print("Account(s) for {} {}".format(self.first, self.last))
+      print("----------------------------------")
       for k, v in self.accounts.items():
-        print("Here are the account(s) for {} {}".format(self.first, self.last))
-        print("----------------------------------")
-        print("{} >>> {}".format(k.capitalize(), v.print_balance()))
+        print("{} >>> {}".format(k.title(), v.print_balance()))
 
 
 class Bank:
+  """
+  Bank class handles the Accounts and Persons.
+  """
 
   def __init__(self):
     self.customers = {}
-    self.savings_interst = 1.07
+    self.savings_interest = 1.07
 
   def new_customer(self, first_name, last_name, email):
     new_customer = Person(first_name, last_name, email)
@@ -104,7 +105,6 @@ class Bank:
   def remove_customer_account(self, email, account_name):
     pass
 
-
 # myAccount = Account(1000, "Michael")
 # myAccount.deposit(50)
 # myAccount.withdraw(75)
@@ -114,10 +114,11 @@ class Bank:
 
 me = Person("Michael", "Williams", "me@gmail.com")
 me.open_account(1000, "Car Fund", "savings")
+me.open_account(2000, "House Fund", "savings")
 # me.show_accounts()
 # me.close_account("Checking")
 # me.show_accounts()
-# me.close_account("savings")
+me.close_account("Car fund")
 # me.show_accounts()
 
 

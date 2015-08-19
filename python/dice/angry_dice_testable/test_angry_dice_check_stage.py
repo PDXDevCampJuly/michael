5b90d8a40f878b774cb8b2da3535af57612c72e6
williamsmichael <willia32@hotmail.com>
2015-08-19 01:42:18 -0700

    design tests for double angrys

--- python/dice/angry_dice_testable/test_angry_dice_check_stage.py
+++ python/dice/angry_dice_testable/test_angry_dice_check_stage.py
@@ -3,14 +3,12 @@ __author__ = 'mw'
 import unittest
 from angry_dice import AngryDice
 from unittest.mock import patch
-from io import StringIO
 
 
-class test_check_stage(unittest.TestCase):
+class TestAngryDiceCheckStage(unittest.TestCase):
 
     def setUp(self):
         """ Initializes the die for the following testing functions """
-
         self.game = AngryDice()
         # print(self.shortDescription())
 
@@ -18,7 +16,7 @@ class test_check_stage(unittest.TestCase):
         """ delete the instance of game after running tests """
         del self.game
 
-    def test_check_stage_aAngry_bAngry_stage2(self):
+    def test_check_stage_stage2_aAngry_bAngry(self):
         """ [Angry, Angry], does stage 2 change aAngry, bAngry """
         self.game.die_a.currentValue = "ANGRY"
         self.game.die_b.currentValue = "ANGRY"
@@ -26,8 +24,9 @@ class test_check_stage(unittest.TestCase):
         self.game.check_stage()
         self.assertEqual(1, self.game.stage,
                             "[Angry, Angry], stage 2 changes to 1")
+        self.assertIn("back to Stage 1", self.game.outcome, "reset to stage 1")
 
-    def test_check_stage_aAngry_bAngry_stage3(self):
+    def test_check_stage_stage3_aAngry_bAngry(self):
         """ [Angry, Angry], does stage 3 change aAngry, bAngry """
         self.game.die_a.currentValue = "ANGRY"
         self.game.die_b.currentValue = "ANGRY"
@@ -35,12 +34,19 @@ class test_check_stage(unittest.TestCase):
         self.game.check_stage()
         self.assertEqual(1, self.game.stage,
                             "[Angry, Angry], stage 3 changes to 1")
+        self.assertIn("back to Stage 1", self.game.outcome, "reset to stage 1")
 
-    def test_check_stage_aAngry_b1_stage2(self):
-        """ [Angry, 1], does stage 2 remain aAngry, b1 """
+    def test_check_stage_stage2_a1_bAngry(self):
+        """ [Angry, 1], does stage 2 remain a1, bAngry """
         self.game.die_a.currentValue = "1"
         self.game.die_b.currentValue = "ANGRY"
         self.game.stage = 2
         self.game.check_stage()
         self.assertNotEqual(1, self.game.stage,
-                            "[Angry, 1], stage 2 still 2")
+                            "[1, Angry], stage 2 still 2")
+        self.assertNotIn("back to Stage 1", self.game.outcome,
+                         "remain on Stage 2")
+
+
+if __name__ == '__main__':
+    unittest.main()

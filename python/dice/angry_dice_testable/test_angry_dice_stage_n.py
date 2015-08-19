f838e5d9f0d5faf6a32112b037698643f3e8c8db
williamsmichael <willia32@hotmail.com>
2015-08-18 23:38:08 -0700

    incorporated testing for outcome statement

--- python/dice/angry_dice_testable/test_angry_dice_stage_n.py
+++ python/dice/angry_dice_testable/test_angry_dice_stage_n.py
@@ -91,24 +91,36 @@ class test_stage_n(unittest.TestCase):
         self.game.finalCheck = False
         self.game.stage_3("5", "6")
         self.assertEqual(True, self.game.finalCheck, "[5, 6], win, game over")
+        self.assertMultiLineEqual("\n-------------------------------" \
+                           "\n-------------------------------" \
+                           "\nYou've won! Calm down!\n", self.game.outcome,
+                                  "win, game over")
 
     def test_stage3_a5_b1(self):
         """ [5, 1] finalCheck stays False """
         self.game.finalCheck = False
         self.game.stage_3("5", "1")
-        self.assertNotEqual(True, self.game.finalCheck, "[5, 1], stays at stage 3")
+        self.assertNotEqual(True, self.game.finalCheck, "[5, 1], "
+                                                        "stays at stage 3")
+        self.assertEqual("", self.game.outcome, "stays at stage 3")
 
     def test_stage3_a6_b5(self):
         """ [5, 6] win, game over """
         self.game.finalCheck = False
         self.game.stage_3("6", "5")
         self.assertEqual(True, self.game.finalCheck, "[6, 5], win, game over")
+        self.assertMultiLineEqual("\n-------------------------------" \
+                           "\n-------------------------------" \
+                           "\nYou've won! Calm down!\n", self.game.outcome,
+                                  "win, game over")
 
     def test_stage3_a1_b1(self):
         """ [1, 1] finalCheck stays False """
         self.game.finalCheck = False
         self.game.stage_3("1", "1")
-        self.assertNotEqual(True, self.game.finalCheck, "[1, 1], stays at stage 3")
+        self.assertNotEqual(True, self.game.finalCheck, "[1, 1], "
+                                                        "stays at stage 3")
+        self.assertEqual("", self.game.outcome, "stays at stage 3")
 
 
 if __name__ == '__main__':

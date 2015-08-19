c8db1ba4e7a7c2f038d68981993bb61e5d5dddc1
williamsmichael <willia32@hotmail.com>
2015-08-19 00:30:34 -0700

    design tests for print_results

--- python/dice/angry_dice_testable/test_angry_dice_print_results.py
+++ python/dice/angry_dice_testable/test_angry_dice_print_results.py
@@ -1,11 +1,38 @@
 __author__ = 'mw'
 
 import unittest
+from angry_dice import AngryDice
+from unittest.mock import patch
+from io import StringIO
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TestAngryDicePrintResults(unittest.TestCase):
+
+    def setUp(self):
+        """ Initializes the die for the following testing functions """
+
+        self.game = AngryDice()
+        # print(self.shortDescription())
+
+    def tearDown(self):
+        """ delete the instance of game after running tests """
+        del self.game
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_stage_output(self, mock_stdout):
+        """ does the stage print """
+        self.game.stage = 1
+        self.game.die_a = "2"
+        self.game.die_b = "3"
+        self.game.outcome = "This is a Test"
+        output = "\nYou are in Stage {}\n" \
+                 "-------------------------------\n" \
+                 "You rolled:\n" \
+                 "a = [  {}  ]\n" \
+                 "b = [  {}  ]\n" \
+                 "{}".format("1", "2", "3", "This is a Test\n")
+        self.game.print_results()
+        self.assertMultiLineEqual(mock_stdout.getvalue(), output)
 
 
 if __name__ == '__main__':

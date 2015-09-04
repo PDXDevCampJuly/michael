8ba2df9bf54145dce8bfc72655c282ff841e7a8d
williamsmichael <willia32@hotmail.com>
2015-09-04 10:30:44 -0700

    minor fixes to ajax call for json

--- html_css_js/pricing/pricing_json/js/script.js
+++ html_css_js/pricing/pricing_json/js/script.js
@@ -185,7 +185,7 @@ newRequest.onload = function() {
         var response = JSON.parse(newRequest.responseText);
         // console.log(response);
         // console.log(response.items);
-        var items = response.items
+        var items = response.inventory.items
 
         for (var i = 0; i < items.length; i++) {
             var product = new Product (

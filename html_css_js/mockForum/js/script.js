7f3df5e98094749e22a86d85e95cab306200c4e7
williamsmichael <willia32@hotmail.com>
2015-09-09 10:38:00 -0700

    removes timeout and adds set interval for GET

--- html_css_js/mockForum/js/script.js
+++ html_css_js/mockForum/js/script.js
@@ -24,7 +24,7 @@ $('form').on('submit', function(e) {
   if ((title !== "") && (body !== "")) {
     $.ajax({
       type: 'POST',
-      dataType: 'xml',
+      dataType: 'json',
       url: 'https://docs.google.com/forms/d/1blH7mM6udvlyJ0SrPmbXoNPZg8XCqDQaxHTPrK0HQbA/formResponse',
       data: { "entry_434124687": title, "entry_1823097801": body },
       statusCode: {
@@ -46,23 +46,26 @@ $('form').on('submit', function(e) {
 })
 
 // AJAX Get - grabs the data from the Google spreadsheet
-$.ajax({
-  type: 'GET',
-  dataType: 'jsonp',
-  crossDomain: true,
-  url: 'https://spreadsheets.google.com/feeds/list/1ntmcFZk4R0Owmez5eKc0bcu_PftAKwWyXDWTqmypPgI/default/public/values?alt=json-in-script',
-  error: function() { console.log("fail.."); },
-  success: function(data) {
-    entries = data.feed.entry;
-    entries.reverse(); // show the last forum post first
-    for (var i = 0; i < entries.length; i++) {
-      var title = entries[i].gsx$posttitle.$t;
-      var body = entries[i].gsx$postbody.$t;
-      createForumPost(title, body);
-    };
-  },
-  timeout: 10000
-});
+function updateForum() {
+  console.log("forum updated..")
+  $.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    crossDomain: true,
+    url: 'https://spreadsheets.google.com/feeds/list/1ntmcFZk4R0Owmez5eKc0bcu_PftAKwWyXDWTqmypPgI/default/public/values?alt=json-in-script',
+    error: function() { console.log("fail.."); },
+    success: function(data) {
+      entries = data.feed.entry;
+      entries.reverse(); // show the last forum post first
+      for (var i = 0; i < entries.length; i++) {
+        var title = entries[i].gsx$posttitle.$t;
+        var body = entries[i].gsx$postbody.$t;
+        createForumPost(title, body);
+      };
+    }
+  });
+}
+setInterval( updateForum, 20000 );
 
 // populates the posts to the webpage for the user to see
 function createForumPost(title, body) {

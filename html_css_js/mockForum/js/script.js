03b7dcf97b3865fdc42ba7ed61a0c57f75b160d3
williamsmichael <willia32@hotmail.com>
2015-09-09 11:11:45 -0700

    removes set interval; changes button to type button

--- html_css_js/mockForum/js/script.js
+++ html_css_js/mockForum/js/script.js
@@ -16,24 +16,22 @@ $('textarea').on('focus', function() {
 })
 
 // submits a post to the Google spreadsheet
-$('form').on('submit', function(e) {
-
-  e.preventDefault();
+$('[type=button]').on('click', function() {
   var title = $('#title').val();
   var body = $('#body').val();
   if ((title !== "") && (body !== "")) {
     $.ajax({
       type: 'POST',
-      dataType: 'json',
+      dataType: 'xml',
       url: 'https://docs.google.com/forms/d/1blH7mM6udvlyJ0SrPmbXoNPZg8XCqDQaxHTPrK0HQbA/formResponse',
       data: { "entry_434124687": title, "entry_1823097801": body },
       statusCode: {
-        0: function (){
+        0: function() {
           $('#title').val("");
           $('#body').val("");
           $msgSuccess.removeClass("hidden").addClass("show");
         },
-        200: function (){
+        200: function() {
           $('#title').val("");
           $('#body').val("");
           $msgSuccess.removeClass("hidden").addClass("show");
@@ -46,26 +44,21 @@ $('form').on('submit', function(e) {
 })
 
 // AJAX Get - grabs the data from the Google spreadsheet
-function updateForum() {
-  console.log("forum updated..")
-  $.ajax({
-    type: 'GET',
-    dataType: 'jsonp',
-    crossDomain: true,
-    url: 'https://spreadsheets.google.com/feeds/list/1ntmcFZk4R0Owmez5eKc0bcu_PftAKwWyXDWTqmypPgI/default/public/values?alt=json-in-script',
-    error: function() { console.log("fail.."); },
-    success: function(data) {
-      entries = data.feed.entry;
-      entries.reverse(); // show the last forum post first
-      for (var i = 0; i < entries.length; i++) {
-        var title = entries[i].gsx$posttitle.$t;
-        var body = entries[i].gsx$postbody.$t;
-        createForumPost(title, body);
-      };
-    }
-  });
-}
-setInterval( updateForum, 20000 );
+$.ajax({
+  type: 'GET',
+  dataType: 'jsonp',
+  url: 'https://spreadsheets.google.com/feeds/list/1ntmcFZk4R0Owmez5eKc0bcu_PftAKwWyXDWTqmypPgI/default/public/values?alt=json-in-script',
+  error: function() { console.log("fail.."); },
+  success: function(data) {
+    entries = data.feed.entry;
+    entries.reverse(); // show the last forum post first
+    for (var i = 0; i < entries.length; i++) {
+      var title = entries[i].gsx$posttitle.$t;
+      var body = entries[i].gsx$postbody.$t;
+      createForumPost(title, body);
+    };
+  }
+});
 
 // populates the posts to the webpage for the user to see
 function createForumPost(title, body) {

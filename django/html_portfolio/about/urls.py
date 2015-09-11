312e4033c03d9dd6c587214b9f7120820a4672ea
williamsmichael <willia32@hotmail.com>
2015-09-11 13:58:35 -0700

    creates about on the django server

--- django/html_portfolio/about/urls.py
+++ django/html_portfolio/about/urls.py
@@ -1,14 +1,8 @@
 from django.conf.urls import url
+from about import views
 
 from . import views
 
 urlpatterns = [
-    # ex: /polls/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /about/5/results/
-    url(r'^results/(?P<question_id>[0-9]+)/$', views.results, name='results'),
-    # ex: /about/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- django/html_portfolio/html_portfolio/urls.py
+++ django/html_portfolio/html_portfolio/urls.py
@@ -6,8 +6,13 @@ urlpatterns = [
     # url(r'^$', 'html_portfolio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^admin/', include(admin.site.urls)),
-    # url(r'^$', views.index, name='index'),
 
+
+    # url(r'^$', views.index, name='index'),
     url(r'^about/', include('about.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^javapic/', include('javapic.urls')),
+    # url(r'^javapic_query/', include('javapic_jquery.urls')),
+    # url(r'^zen_mockup/', include('zen_mockup.urls')),
+    # url(r'^forum/', include('forum.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
 ]

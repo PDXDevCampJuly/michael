52e007af2b06b2fd1fde9f31c38aa46a1884d3c2
williamsmichael <willia32@hotmail.com>
2015-09-11 02:21:59 -0700

    modifications from original project; updates files

--- django/hello_world/hello/static/js/scripts.js
+++ django/hello_world/hello/static/js/scripts.js
@@ -5,42 +5,37 @@ var $forumWrapper = $('#forumWrapper');
 var $msgSuccess = $('.text-success');
 var $msgDanger = $('.text-danger');
 
-  // hide helper messages
-$('input').on('focus', function() {
-  $msgSuccess.removeClass("show").addClass("hidden");
-  $msgDanger.removeClass("show").addClass("hidden");
-});
-$('textarea').on('focus', function() {
-  $msgSuccess.removeClass("show").addClass("hidden");
-  $msgDanger.removeClass("show").addClass("hidden");
+  // hide helper message(s)
+$('input, textarea').on('focus', function() {
+  $msgSuccess, $msgDanger.removeClass("show").addClass("hidden");
 });
 
 // submits a post to the Google spreadsheet
-$('[type=button]').on('click', function() {
-  var title = $('#title').val();
-  var body = $('#body').val();
-  if ((title !== "") && (body !== "")) {
+$('button').on('click', function(event) {
+  var $title = $('input').val();
+  var $body = $('textarea').val();
+  if (($title !== "") && ($body !== "")) {
     $.ajax({
       type: "POST",
       dataType: "xml",
       url: "https://docs.google.com/forms/d/1blH7mM6udvlyJ0SrPmbXoNPZg8XCqDQaxHTPrK0HQbA/formResponse",
-      data: { "entry_434124687": title, "entry_1823097801": body },
+      data: { "entry_434124687": $title, "entry_1823097801": $body },
       statusCode: {
         0: function() {
+          //success msg (refer to index.HTML)
+          $msgSuccess.removeClass("hidden").addClass("show");
           location.reload();
         },
         200: function() {
+          $msgSuccess.removeClass("hidden").addClass("show");
           location.reload();
         }
       }
     });
 
     //clear input fields
-    $('#title').val("");
-    $('#body').val("");
-
-    //success msg (refer to index.HTML)
-    $msgSuccess.removeClass("hidden").addClass("show");
+    $('input').val("");
+    $('textarea').val("");
 
   } else {
     //error msg (refer to index.HTML)
@@ -49,26 +44,39 @@ $('[type=button]').on('click', function() {
 });
 
 // AJAX Get - grabs the data from the Google spreadsheet
-$.ajax({
-  type: 'GET',
-  dataType: 'jsonp',
-  url: 'https://spreadsheets.google.com/feeds/list/1ntmcFZk4R0Owmez5eKc0bcu_PftAKwWyXDWTqmypPgI/default/public/values?alt=json-in-script',
-  error: function() { console.log("fail.."); },
-  success: function(data) {
-    entries = data.feed.entry;
-    entries.reverse(); // show the last forum post first
-    for (var i = 0; i < entries.length; i++) {
-      var title = entries[i].gsx$posttitle.$t;
-      var body = entries[i].gsx$postbody.$t;
-      createForumPost(title, body);
+(function refreshPosts() {
+  $.ajax({
+    type: 'GET',
+    dataType: 'jsonp',
+    url: 'https://spreadsheets.google.com/feeds/list/1ntmcFZk4R0Owmez5eKc0bcu_PftAKwWyXDWTqmypPgI/default/public/values?alt=json-in-script',
+    error: function() { console.log("fail.."); },
+    success: function(data) {
+      entries = data.feed.entry;
+      entries.reverse(); // show the last forum post first
+      for (var i = 0; i < entries.length; i++) {
+        var title = entries[i].gsx$posttitle.$t;
+        var body = entries[i].gsx$postbody.$t;
+        createForumPost(title, body);
+      }
     }
-  }
-});
+  }).then(function() {
+    setTimeout(refreshPosts, 15000);
+  });
+}());
 
 // creates elements for DOM manipulation
 // populates the posts to the webpage for the user to see
 function createForumPost(title, body) {
-  $forumWrapper.append('<div class="alert alert-warning" role="alert"><h4>' + title + '</h4><p>' + body + '</p></div>').hide().fadeIn('slow');
+  $forumWrapper.append('<div class="alert alert-warning" role="alert"><h4>' + title + '</h4><p>' + body + '</p></div>');
 }
 
-
+// // media queries for bootstrap classes
+if (window.matchMedia("(min-width: 992px)").matches) {
+  /* the viewport is at least 992 pixels wide */
+  $('input, textarea').removeClass('input-md').addClass('input-lg');
+  $('button').removeClass('btn-md').addClass('btn-lg');
+} else {
+  /* the viewport is less than 992 pixels wide */
+  $('input, textarea').removeClass('input-lg').addClass('input-md');
+  $('button').removeClass('btn-lg').addClass('btn-md');
+}

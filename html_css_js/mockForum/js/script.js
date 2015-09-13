36f4ac4e0a678871205b1b1e2f2951ab37202f1b
williamsmichael <willia32@hotmail.com>
2015-09-13 01:12:03 -0700

    removes success msg

--- html_css_js/mockForum/js/script.js
+++ html_css_js/mockForum/js/script.js
@@ -2,16 +2,33 @@
 // Utilizing Google Spreadsheets API
 
 var $forumWrapper = $('#forumWrapper');
-var $msgSuccess = $('.text-success');
 var $msgDanger = $('.text-danger');
 
-  // hide helper message(s)
-$('input, textarea').on('focus', function() {
-  $msgSuccess, $msgDanger.removeClass("show").addClass("hidden");
-});
+// AJAX Get - grabs the data from the Google spreadsheet
+$.ajax({
+  type: 'GET',
+  dataType: 'jsonp',
+  url: 'https://spreadsheets.google.com/feeds/list/1ntmcFZk4R0Owmez5eKc0bcu_PftAKwWyXDWTqmypPgI/default/public/values?alt=json-in-script',
+  error: function() { console.log("fail.."); },
+  success: function(data) {
+    entries = data.feed.entry;
+    entries.reverse(); // show the last forum post first
+    for (var i = 0; i < entries.length; i++) {
+      var title = entries[i].gsx$posttitle.$t;
+      var body = entries[i].gsx$postbody.$t;
+      createForumPost(title, body);
+    }
+  }
+})
+
+// creates elements for DOM manipulation
+// populates the posts to the webpage for the user to see
+function createForumPost(title, body) {
+  $forumWrapper.append('<div class="alert alert-warning" role="alert"><h4>' + title + '</h4><p>' + body + '</p></div>');
+}
 
 // submits a post to the Google spreadsheet
-$('button').on('click', function(event) {
+$( 'button' ).on( 'click', function() {
   var $title = $('input').val();
   var $body = $('textarea').val();
   if (($title !== "") && ($body !== "")) {
@@ -19,21 +36,16 @@ $('button').on('click', function(event) {
       type: "POST",
       dataType: "xml",
       url: "https://docs.google.com/forms/d/1blH7mM6udvlyJ0SrPmbXoNPZg8XCqDQaxHTPrK0HQbA/formResponse",
-      data: { "entry_434124687": $title, "entry_1823097801": $body },
+      data: {"entry_434124687": $title, "entry_1823097801": $body},
       statusCode: {
-        0: function() {
-          //success msg (refer to index.HTML)
-          $msgSuccess.removeClass("hidden").addClass("show");
+        0: function () {
+          location.reload();
         },
-        200: function() {
-          $msgSuccess.removeClass("hidden").addClass("show");
+        200: function () {
+          location.reload();
         }
       }
     });
-
-    //clear input fields
-    $('input').val("");
-    $('textarea').val("");
 
   } else {
     //error msg (refer to index.HTML)
@@ -41,32 +53,11 @@ $('button').on('click', function(event) {
   }
 });
 
-// AJAX Get - grabs the data from the Google spreadsheet
-(function refreshPosts() {
-  $.ajax({
-    type: 'GET',
-    dataType: 'jsonp',
-    url: 'https://spreadsheets.google.com/feeds/list/1ntmcFZk4R0Owmez5eKc0bcu_PftAKwWyXDWTqmypPgI/default/public/values?alt=json-in-script',
-    error: function() { console.log("fail.."); },
-    success: function(data) {
-      entries = data.feed.entry;
-      entries.reverse(); // show the last forum post first
-      for (var i = 0; i < entries.length; i++) {
-        var title = entries[i].gsx$posttitle.$t;
-        var body = entries[i].gsx$postbody.$t;
-        createForumPost(title, body);
-      }
-    }
-  }).then(function() {
-    setTimeout(refreshPosts, 15000);
-  });
-}());
+// hide helper message(s) if already displayed
+$( 'input, textarea' ).on( 'focus', function() {
+  $msgDanger.removeClass("show").addClass("hidden");
+});
 
-// creates elements for DOM manipulation
-// populates the posts to the webpage for the user to see
-function createForumPost(title, body) {
-  $forumWrapper.append('<div class="alert alert-warning" role="alert"><h4>' + title + '</h4><p>' + body + '</p></div>');
-}
 
 // // media queries for bootstrap classes
 if (window.matchMedia("(min-width: 992px)").matches) {

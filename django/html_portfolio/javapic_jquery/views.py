bea96450228e3b21b1136fc8f68c764485fde7f2
williamsmichael <willia32@hotmail.com>
2015-09-14 23:56:20 -0700

    changes the path to go to join or gallery, not join/gallery

--- django/html_portfolio/javapic_jquery/views.py
+++ django/html_portfolio/javapic_jquery/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 def javapic_jquery(request):
+    """ main page with image slider """
     return render(request, 'javapic_jquery.html')
 
 def join(request):
+    """ join page to access gallery """
     return render(request, 'javapic_jquery_join.html')
 
 def gallery(request):
+    """ gallery of images with lightbox effect """
     return render(request, 'javapic_jquery_gallery.html')

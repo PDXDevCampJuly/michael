3405019406e11df5271c51da61478970e08abd09
williamsmichael <willia32@hotmail.com>
2015-11-13 14:47:41 -0800

    moves pair programming projects to python directory

--- python/ChelsDevCamp_July27/django/portfolioProjrct/forumproject/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def forum(request):
-    return render(request, 'forumHtml.html')
--- python/ChelsDevCamp_July27/django/portfolioProjrct/zenmockupjqproject/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def zen(request):
-    return render(request, 'zen_mockup.html')
